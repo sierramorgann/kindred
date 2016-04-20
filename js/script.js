1ee9820e53161fda2f41bcd87dccca1ff5e86e1b
Sierra Morgan <sierramorgan07@gmail.com>
2016-04-20 08:18:35 -0700

    new new

--- js/script.js
+++ js/script.js
@@ -11,29 +11,27 @@ $(document).ready(function() {
 	});
 
 	$('.mix').on('click', function() {
-        // Get data attribute from clicked header
         var correspondingDiv = $(this).attr('data-divider');
-        // Hide any open 'myDivider' dividers
-        $('.myDivider').each(function() { $(this).hide(); })
+        $('.myDivider').each(function() { $(this).hide(); });
         $('.exit').on('click', function() { $('.myDivider').hide(); })
-        // Display the corresponding divider
         $(correspondingDiv).show();
     });
 
-	$( ".cross" ).hide();
-	$( ".menu" ).hide();
-	$( ".hamburger" ).click(function() {
-	$( ".menu" ).slideToggle( "slow", function() {
-	$( ".hamburger" ).hide();
-	$( ".cross" ).show();
-	});
+	$( '.cross' ).hide();
+	$( '.menu' ).hide();
+		$( '.hamburger' ).on('click', function() {
+			$( '.menu' ).slideToggle( "slow", function() {
+				$( '.hamburger' ).hide();
+				$( '.cross' ).show();
+			});
+		});
+
+	$( '.cross' ).on('click',function() {
+		$( '.menu' ).slideToggle( "slow", function() {
+			$( '.cross' ).hide();
+			$( '.hamburger' ).show();
+		});
 	});
 
-	$( ".cross" ).click(function() {
-	$( ".menu" ).slideToggle( "slow", function() {
-	$( ".cross" ).hide();
-	$( ".hamburger" ).show();
-	});
-	});
 });
 
